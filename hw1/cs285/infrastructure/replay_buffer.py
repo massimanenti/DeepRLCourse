7636cd43818db38c52570ea97c03565d92e250ed
massimanenti <massimanenti.mm@gmail.com>
2024-06-21 13:37:45 +0200

    Commented replay_buffer

--- hw1/cs285/infrastructure/replay_buffer.py
+++ hw1/cs285/infrastructure/replay_buffer.py
@@ -2,6 +2,31 @@ from cs285.infrastructure.utils import *
 
 
 class ReplayBuffer(object):
+    """
+    Defines a ReplayBuffer, in which information of past paths is stored.
+
+    Attributes
+    ----------
+    max_size: int
+        Max size of the buffer
+    paths: list of dictionaries
+        Past rollouts
+    obs: numpy.ndarray
+        Concatenated observation array of all the past rollouts
+    acs: numpy.ndarray
+        Concatenated actions array of all the past rollouts
+    rews: numpy.ndarray
+        Concatenated rewards array of all the past rollouts
+    next_obs: numpy.ndarray
+        Concatenated next_observation array of all the past rollouts
+    terminals: numpy.ndarray
+        Concatenated terminals array of all the past rollouts
+        
+    Methods
+    -------
+    add_rollouts:
+        Add a new rollout
+    """
 
     def __init__(self, max_size=1000000):
 
@@ -24,8 +49,9 @@ class ReplayBuffer(object):
             return 0
 
     def add_rollouts(self, paths, concat_rew=True):
-
         # add new rollouts into our list of rollouts
+
+        # append the list of dictionaries (i.e., paths) to the internal variable (i.e., self.paths)
         for path in paths:
             self.paths.append(path)
 
@@ -41,14 +67,17 @@ class ReplayBuffer(object):
         # the info present in the list of dictionaries "path".
 
         if self.obs is None:
+            # add all the elements
             self.obs = observations[-self.max_size:]
             self.acs = actions[-self.max_size:]
             self.rews = rewards[-self.max_size:]
             self.next_obs = next_observations[-self.max_size:]
             self.terminals = terminals[-self.max_size:]
         else:
+            # concatenate the new elements
             self.obs = np.concatenate([self.obs, observations])[-self.max_size:]
             self.acs = np.concatenate([self.acs, actions])[-self.max_size:]
+
             if concat_rew:
                 self.rews = np.concatenate(
                     [self.rews, rewards]
@@ -59,6 +88,7 @@ class ReplayBuffer(object):
                 else:
                     self.rews.append(rewards)
                 self.rews = self.rews[-self.max_size:]
+
             self.next_obs = np.concatenate(
                 [self.next_obs, next_observations]
             )[-self.max_size:]
